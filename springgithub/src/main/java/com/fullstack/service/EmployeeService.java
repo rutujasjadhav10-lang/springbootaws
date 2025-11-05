4bac9fe8e17b6315d9402af0805d834ecc614350
Rutuja Jadhav <rutujasjadhav10@gmail.com>
2025-11-05 16:27:46 +0530

    add code in employeeservice and iemployeeservice

--- springgithub/src/main/java/com/fullstack/service/EmployeeService.java
+++ springgithub/src/main/java/com/fullstack/service/EmployeeService.java
@@ -1,4 +1,73 @@
 package com.fullstack.service;
 
-public class EmployeeService {
+
+import com.fullstack.entity.Employee;
+import com.fullstack.exception.RecordNotFoundException;
+import com.fullstack.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeService implements IEmployeeService {
+    private  final EmployeeRepository employeeRepository;
+
+    @Override
+    public Employee signUp(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @Override
+    public Boolean signIn(String empEmailId, String empPassword) {
+        boolean flag= false;
+        Employee employee = employeeRepository.findByEmpEmailIdAndEmpPassword(empEmailId,empPassword);
+        if(employee!=null){
+            flag=true;
+        }
+
+        return flag;
+    }
+
+    @Override
+    public List<Employee> saveAll(List<Employee> employeeList) {
+        return employeeRepository.saveAll(employeeList);
+    }
+
+    @Override
+    public Optional<Employee> findById(long empId) {
+        return Optional.ofNullable(employeeRepository.findById(empId).orElseThrow(() -> new RecordNotFoundException("#Emp Id not found")));
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee update(Employee employee, long empId) {
+        Employee employee1 = findById(empId).get();
+        employee1.setEmpPassword(employee.getEmpPassword());
+        employee1.setEmpAddress(employee.getEmpAddress());
+        employee1.setEmpDOB(employee.getEmpDOB());
+        employee1.setEmpName(employee.getEmpName());
+        employee1.setEmpSalary(employee.getEmpSalary());
+        employee1.setEmpEmailId(employee.getEmpEmailId());
+        employee1.setEmpContactNumber(employee.getEmpContactNumber());
+        return employeeRepository.save(employee1);
+    }
+
+    @Override
+    public void deleteById(long empId) {
+        employeeRepository.deleteById(empId);
+
+    }
+
+    @Override
+    public void deleteAll() {
+        employeeRepository.deleteAll();
+
+    }
 }
--- springgithub/src/main/java/com/fullstack/service/IEmployeeService.java
+++ springgithub/src/main/java/com/fullstack/service/IEmployeeService.java
@@ -1,4 +1,25 @@
 package com.fullstack.service;
 
+import com.fullstack.entity.Employee;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IEmployeeService {
+
+    Employee  signUp(Employee employee);
+
+    Boolean signIn( String empEmailId,String empPassword);
+
+    List<Employee> saveAll(List<Employee> employeeList);
+
+    Optional<Employee> findById(long empId);
+
+    List<Employee> findAll();
+
+    Employee update(Employee employee,long empId);
+
+    void deleteById(long empId);
+
+    void deleteAll();
 }
